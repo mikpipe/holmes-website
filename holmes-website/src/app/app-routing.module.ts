1c05617472d1d1cefd8194dd2fcabbc1990f8e78
26918 <Jonas-Josef.Holm@hsrw.org>
2022-02-22 19:15:31 +0100

    Routing

--- holmes-website/src/app/app-routing.module.ts
+++ holmes-website/src/app/app-routing.module.ts
@@ -1,11 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
+import { MusicComponent } from './music/music.component';
+import { SoftwareComponent } from './software/software.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: 'music',
+    component: MusicComponent,
+  },
+  {
+    path: 'software',
+    component: SoftwareComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
   },
 ];
 
